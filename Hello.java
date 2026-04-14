c8bad0f96017ffdacd3603192100c1d79b114870
Guy First <guyfirst19@gmail.com>
2026-04-14 11:01:16 +0300

    fibonacci

--- Hello.java
+++ Hello.java
@@ -1,7 +1,28 @@
-public class Hello {
+import java.util.Random;
+
+public class RandomFibonacci {
     public static void main(String[] args) {
-        for (int i = 1; i <= 15; i++) {
-            System.out.println("Hello World ... " + i);
+        Random random = new Random();
+
+        int n = random.nextInt(30) + 1; // 1 to 30
+
+        long result = fibonacci(n);
+
+        System.out.println("Calculated the " + n + "th Fibonacci number: " + result);
+    }
+
+    public static long fibonacci(int n) {
+        if (n == 1) return 0;
+        if (n == 2) return 1;
+
+        long a = 0, b = 1, c = 0;
+
+        for (int i = 3; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
         }
+
+        return b;
     }
 }
